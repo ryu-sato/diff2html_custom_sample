48d0f8fc1696779b4722fb0c2526d7583060c10c
Ryu Sato <ryu@weseek.co.jp>
2019-01-27 04:59:27 +0900

    コメント追加ボタンの表示非表示が出来るよう修正

--- app/javascript/DiffDetail.vue
+++ app/javascript/DiffDetail.vue
@@ -3,13 +3,14 @@
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 import { Diff2Html } from "diff2html";
 import "diff2html/dist/diff2html.min.css";
 
-var diff_id = location.pathname.replace(/\/diffs\/(\d+)[^\d]*/, '$1');
+var diff_id = location.pathname.replace(/\/diffs\/(\d+)[^\d]*/, "$1");
 var rawTemplates = {
-  'side-by-side-file-diff': (function () {/*
+  "side-by-side-file-diff": function() {
+    /*
     <div id="{{fileHtmlId}}" class="d2h-file-wrapper" data-lang="{{file.language}}">
         <div class="d2h-file-header">
             {{{filePath}}}
@@ -35,78 +36,177 @@ var rawTemplates = {
             </div>
         </div>
     </div>
-    */}).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, ""),
-  'generic-line': (function () {/*
-    <tr>
+    */
+  }
+    .toString()
+    .match(/(?:\/\*(?:[\s\S]*?)\*\/)/)
+    .pop()
+    .replace(/^\/\*/, "")
+    .replace(/\*\/$/, ""),
+  "generic-line": function() {
+    /*
+    <diff-tr>
       <td class="{{lineClass}} {{type}}">
-        !{{{lineNumber}}}
+        {{{lineNumber}}}
       </td>
-      <td class="{{type}}">
+      <code-td line="{{lineNumber}}" class="{{type}}">
         <div class="{{contentClass}} {{type}}">
-          <diff-line>
-            {{#prefix}}
-                <span class="d2h-code-line-prefix">{{{prefix}}}</span>
-            {{/prefix}}
-            {{#content}}
-                <span class="d2h-code-line-ctn">{{{content}}}</span>
-            {{/content}}
-          </diff-line>
+          {{#prefix}}
+              <span class="d2h-code-line-prefix">{{{prefix}}}</span>
+          {{/prefix}}
+          {{#content}}
+              <span class="d2h-code-line-ctn">{{{content}}}</span>
+          {{/content}}
         </div>
+      </code-td>
+    </diff-tr>
+  */
+  }
+    .toString()
+    .match(/(?:\/\*(?:[\s\S]*?)\*\/)/)
+    .pop()
+    .replace(/^\/\*/, "")
+    .replace(/\*\/$/, "")
+};
+
+/**
+ * イベントを通知させるための空コンポーネント
+ */
+var EventBus = new Vue();
+
+/**
+ * コメント追加ボタン用コンポーネント
+ */
+var AddBtn = Vue.component("add-btn", {
+  props: {
+    line: Number
+  },
+  template: '<button class="btn dcs-add-comment" @click="showCommentForm">&#43;</button>',
+  methods: {
+    showCommentForm: function() {
+      EventBus.$emit('show-comment-form', this.line, -1);
+    }
+  }
+});
+
+/**
+ * Diffテーブルのコード表示列コンポーネント
+ */
+var CodeTD = Vue.component("code-td", {
+  props: {
+    line: String  // Numberにすると空文字列の場合にエラーとなるので一旦文字列で受け取っている
+  },
+  data() {
+    return {
+      btnSeen: false
+    };
+  },
+  components: {
+    AddBtn
+  },
+  template:
+    '<td @mouseenter="mouseEnter" @mouseleave="mouseLeave">' +
+      '<add-btn refs="addBtn" v-if="btnSeen" v-bind:line="parseInt(line)"></add-btn>' +
+      '<slot></slot>' +
+      '</td>',
+  methods: {
+    mouseEnter: function() {
+      if (this.line == "") {
+        return;  // 行番号が無い場合はコメント追加できないので追加ボタンを表示させないで関数を終わらせる
+      }
+      this.btnSeen = true;
+    },
+    mouseLeave: function() {
+      this.btnSeen = false;
+    }
+  }
+});
+
+/**
+ * コメント挿入フォーム
+ */
+var CmtForm = Vue.component("comment-form", {
+  props: {
+    commentId: Number,
+    line: Number,
+    diffId: Number,
+    content: String,
+  },
+  template: `
+    <tr>
+      <td colspan="2">
+        <form id={"edit-comment-" + this.line} class="edit-comment" action={"/comments/" + this.commentId} method="post">
+          <div class="card">
+            <div class="card-header">Comment</div>
+            <div class="card-body">
+              <div class="form-group">
+                <input type="hidden" name="_method" value="patch" />
+                <input type="hidden" name="comment[line]" value={this.line} />
+                <input type="hidden" name="comment[diff_id]" value={this.diffId} />
+                <textarea name="comment[content]" class="form-control mb-3" id="comment" placeholder="Leave a comment">
+                  {this.content}
+                </textarea>
+                <button class="btn btn-outline-secondary mr-2">Cancel</button>
+                <input type="submit" class="btn btn-success" value="Update" />
+              </div>
+            </div>
+          </div>
+        </form>
       </td>
     </tr>
-  */}).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "")
-};
+  `
+})
 
-var AddBtn = Vue.component('add-btn', {
-    template: '<button class="btn dcs-add-comment" @click=onClick>&#43;</button>',
-    methods: {
-        onClick: function() {
-            alert('add-btn');
-        }
+/**
+ * Diffテーブルの行コンポーネント
+ */
+var DiffTR = Vue.component("diff-tr", {
+  data() {
+    return {
+      formSeen: false
     }
+  },
+  template: "<tr><slot></slot></tr>",
 });
 
-var DiffLine = Vue.component('diff-line', {
-    data () {
-        return {
-            seen: true
-        }
-    },
-    components: {
-        AddBtn
-    },
-    template: '<div @mouseenter="mouseEnter" @mouseleave="mouseLeave"><add-btn refs="addBtn" v-if="seen"></add-btn></div>',
-    methods: {
-        mouseEnter: function() {
-            this.seen = true;
-        },
-        mouseLeave: function() {
-            this.seen = false;
-        }
-    }
-});
-
-
+/**
+ * 全行Diff用コンポーネント
+ */
 export default {
-    props: ['diffString'],
-    data () {
-        return {
-            compiled: null
-        }
+  props: ["diffString"],
+  data() {
+    return {
+      compiled: null
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      console.log("this.diffString");
+      console.log(this.diffString);
+      this.compiled = Vue.compile(
+        "<div>" +
+          Diff2Html.getPrettyHtml(this.diffString, {
+            inputFormat: "diff",
+            showFiles: true,
+            matching: "lines",
+            outputFormat: "side-by-side",
+            rawTemplates: rawTemplates
+          }) +
+          "</div>"
+      );
+    }, 500);
+  },
+  created() {
+    EventBus.$on('show-comment-form', this.showCommentForm),
+    EventBus.$on('hide-comment-form', this.hideCommentForm)
+  },
+  methods: {
+    showCommentForm(codeLine, trIndex) {
+      alert("showCommentForm: " + codeLine + ", " + trIndex);
     },
-    mounted(){ 
-        setTimeout(() => { 
-            console.log("this.diffString");
-            console.log(this.diffString);
-            this.compiled = Vue.compile('<div>' + Diff2Html.getPrettyHtml(this.diffString, {
-                inputFormat: "diff",
-                showFiles: true,
-                matching: "lines",
-                outputFormat: "side-by-side",
-                rawTemplates: rawTemplates
-                }) + '</div>');
-        }, 500)
+    hideCommentForm(codeLine, trIndex) {
+      alert("hideCommentForm: " + codeLine + ", " + trIndex);
     }
+  }
 };
-
 </script>
